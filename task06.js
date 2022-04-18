60da7bcf192bcbb55c5e2bf3711e0523c470737c
Keaobaka Makete <keaobaka.km@gmail.com>
2022-04-19 01:41:00 +0200

    task 6

---
+++ task06.js
@@ -0,0 +1,12 @@
+
+function maximumNumber(a,b,c){
+    if (a > b && a > c){
+        console.log(a);
+    }
+    else if(b > a && b > c){
+        console.log(b);
+    }else{
+        console.log(c);
+    }
+} 
+maximumNumber(11,1,12);
