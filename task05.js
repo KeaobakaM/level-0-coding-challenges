aad6b7a5b2b2ff51c9d36bfc03ec1edac4ed9f1f
Keaobaka Makete <keaobaka.km@gmail.com>
2022-04-19 01:40:24 +0200

    task 05

---
+++ task05.js
@@ -0,0 +1,8 @@
+
+function areaOfTriangle(sidea,sideb,sidec){
+    let area = (.5 * (sidea + sideb + sidec));
+   
+    console.log("The area of the triangle is " + area);
+    
+} 
+areaOfTriangle(5,8,9);
