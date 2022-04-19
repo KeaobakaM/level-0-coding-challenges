12c091097f154b4a439d8aae69b914f83a55a661
Keaobaka Makete <keaobaka.km@gmail.com>
2022-04-19 19:44:36 +0200

    change task 8

--- task08.js
+++ task08.js
@@ -1,16 +1,20 @@
 
 function hoursAndMinutes(time){
-    var h = time / 60;
-    var hours = parseInt(h);                 //so that hours won't give any remainders 
-    var minutes = time % 60;
-    if(hours == 1){
-        console.log(hours + " hour, " + minutes + " minutes");
+    let total = time / 60;
+    let hours = parseInt(total);                 //so that hours won't give any remainders 
+    let minutes = time % 60;
+    let hourWord;
+    let minuteWord;
+
+    if (hours == 1){
+        hourWord = " hour ";
     }
-    else if(hours > 1){
-        console.log(hours + " hours, " + minutes + " minutes");
-    }else{
-        console.log(minutes + " minutes")
-    }
+    else {hourWord = " hours "}
     
+    if (minutes == 1){
+        minuteWord = " minute";
+    }
+    else {minuteWord = " minutes"};
+    console.log(hours + hourWord + minutes + minuteWord);
 }
-hoursAndMinutes(230);
+hoursAndMinutes(61);
