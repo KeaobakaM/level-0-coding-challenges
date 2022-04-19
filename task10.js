4b064fde67f8485827b41e34676610b6a677949e
Keaobaka Makete <keaobaka.km@gmail.com>
2022-04-19 19:45:54 +0200

    change task 10

--- task10.js
+++ task10.js
@@ -1,16 +1,18 @@
 
 function commonCharacters(string1, string2){
-    let commonCharArray = ["Common letters: "];
     
+    let commonCharArray = [];
     let array1 = Array.from(string1);
     let array2 = Array.from(string2);
 
     for(i = 0;i < array1.length; i++){
         if (array2.indexOf(array1[i]) != -1){   // -1 means that the element is not there
-            commonCharArray.push(array1[i]);    // will push element if it's not -1 (b will not be pushed)
+            commonCharArray.push(" " + array1[i]);    // will push element if it's not -1 (b will not be pushed)
         }
     }
-    console.log(commonCharArray);
+    
+    let commonCharString = commonCharArray.toString();
+    console.log("Common letters: " + commonCharString);
     
 }
-commonCharacters("bale", "manicure");
+commonCharacters("baile", "manicure");
